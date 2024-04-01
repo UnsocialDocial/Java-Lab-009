08c1db92c85826669aebfa45360ae01d3eaa51dd
Brody <brodyballiett@gmail.com>
2024-03-31 23:28:01 -0700

    lab 009

--- src/BigIntRewrite.java
+++ src/BigIntRewrite.java
@@ -1,5 +1,22 @@
-public class BigIntRewrite {
-    public static void main(String[] args) {
+import java.math.BigInteger;
 
+public class BigIntRewrite {
+    public static BigInteger pow(int x, int n) {
+        if (n == 0) return BigInteger.ONE;
+
+        BigInteger t = pow(x, n / 2);
+
+        if (n % 2 == 0) {
+            return t.multiply(t);
+        } else {
+            return t.multiply(t).multiply(BigInteger.valueOf(x));
+        }
+    }
+    public static void main(String[] args) {
+        // Test cases
+        int x = 2;
+        int n = 10;
+        BigInteger result = pow(x, n);
+        System.out.println(x + " raised to the power of " + n + " is: " + result);
     }
 }
